3cc32e0a0077180fb5cfd1ab42c20130aa1831ee
hilatsivion <hilatsivion2@gmail.com>
2025-04-27 13:18:49 +0300

    init

---
+++ prediction-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Prediction Dashboard</h1>
+      <p>Start predicting now!</p>
+    </div>
+  );
+}
+
+export default App;
